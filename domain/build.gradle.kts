48cf72110950f610a0b11ad59f30f4e592200dc2
Denis Zabolotski <morsmes@gmail.com>
2023-08-22 22:04:22 +0300

    Initial commit, convert gradle to kts

---
+++ domain/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("java-library")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
